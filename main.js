c630038d2bccda6b1ca952cbd8d84e322acd6af2
Ania <an.pasieczna@gmail.com>
2022-07-21 12:48:22 +0200

    excercises on arrays- every, some, merging, etc

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 //MAP. Let's return an array with even numbers only. Remember, that map returns "undefined" for odd numbers in the old array.
-const numbersArr =[1, 2, 3, 4, 5, 10, 503, 408, 0]
+const numbersArr =[1, 2, 3, 4, 5, 10, 503, 408, 0, 83]
 
 const evenNumbersWithMap = numbersArr.map((item) => {
     if(item % 2 === 0) {
-        return item
+    return item
     } else {
         
         return item + 1
     }
 }) 
 console.log('even numbers with MAP', evenNumbersWithMap)
-//Let's shpw evene numbers yousing a loop
-for(let i = 0; i < numbersArr.length; i++) {
-    if(i % 2 === 0) {
-        console.log('even numbers with a loop', numbersArr[i])
-    }
-
+function testujeSeMapa(jakasTablica) {
+    const NowaTablicaPoMapie = jakasTablica.map((currentValue) =>{
+        return currentValue +1
+    })
 }
+
  
 //Lets's do the same with a FILTER
 
@@ -35,8 +34,26 @@ const oddNumbersAfterFilter = numbersArr.filter((item) =>{
 })
 
 console.log('Im showing odd numbers after  FILTER', oddNumbersAfterFilter)
+//Let's show even numbers sing PUSH
 
-//Check, if the numbersArray includes a givennumber. use INCLUDES
+let newNumbersArrWithEvenNumbers = []
+for(let i = 0; i < numbersArr.length; i++) {
+    if(numbersArr[i] % 2 === 0) {
+        newNumbersArrWithEvenNumbers.push(numbersArr[i])
+        console.log('New arrays with even numbers after PUSH ',  newNumbersArrWithEvenNumbers)
+
+    }
+}
+
+//How about showing even numbers with forEach?
+
+const evenNumbersForEach = numbersArr.forEach((item) =>{
+    if(item % 2 === 0) {
+        console.log(item)   
+    } else console.log('this number is odd')
+})
+
+//Check, if the numbersArray includes a given number. use INCLUDES
 
 const hasNuberFifty = numbersArr.includes(50)
 console.log('Has number 50? ', hasNuberFifty)
@@ -57,11 +74,98 @@ const has50 = hasTheWord(numbersArr, 50)
 
 //Write a function that would find a word/element in a text.
 
-function findElement(someArray, item) {
-    const isThereElement = someArray.find((item) =>{ 
-        //Alternative transcription someArray(item => item === 3), no return!
-        console.log('we have it')
-        return item ===3
-    }) 
+
+    const findSomeElement = numbersArr.find((someElement) =>{
+        return someElement === 3
+        })
+   
+console.log('I am returing an element after FIND: ', findSomeElement)
+
+
+//Delete the last element of any arry using POP
+
+const arrayWithPop = numbersArr.pop()
+
+console.log('New array with last element deleted-POP :', arrayWithPop)
+//Add an element to an array.
+
+const arrayAfterPush = numbersArr.push(999)
+console.log('New array after PUSH has', arrayAfterPush, 'elements')
+
+// Add an element to the beginning of an array: UNSHIFT
+
+function addElementUnshift(arr1, element1) {
+    const arrayAfterUnshift = arr1.unshift(element1) 
+    console.log('the new array after UNSHIFT has now ', arrayAfterUnshift, 'elements')
+    return arrayAfterUnshift
 }
-const isThere3 = findElement(numbersArr)
+
+const newArrAfterUnshift = addElementUnshift(numbersArr, 1111) 
+
+//Remove the first element from any array: SHIFT
+ function removeWithShift(exampleArray) {
+    const whatIremove = exampleArray.shift() 
+    return whatIremove
+   
+   
+ }
+ const whatIsRemoved = addElementUnshift(numbersArr)
+ console.log('The removed element after SHIFT is', whatIsRemoved)
+
+//Let's join any 2 or more arrays using CONCAT
+
+ const names1 = ['John', 'Mary', 'James', 'Thomas', 'William']
+ const names2= ['Peter', 'Steven', 'Elisabeth', 'Jane' , 'Anna']
+
+ function iJoinArrays(anyArrayToBeJoined1, anyArrayToBeJoined2) {
+ const joinedArrays=  anyArrayToBeJoined1.concat(anyArrayToBeJoined2)
+ return joinedArrays
+
+ }
+
+const joinedNames = iJoinArrays(names1, names2)
+console.log('New joined array:', joinedNames)
+
+//How about doing the same using UNSHIFT?
+
+function arraysTogetherUnshift(arrayToBeUnshifted1, arrayToBeUnshifted2)  { 
+    const joining2arraysUnshift = arrayToBeUnshifted1.unshift(arrayToBeUnshifted2)
+    return joining2arraysUnshift
+}
+
+const twoNamesArrraysAFterUnshift = arraysTogetherUnshift(names1, names2)
+console.log('joining arrays with UNSHIFT', twoNamesArrraysAFterUnshift)
+
+//Using a LOOP to join two arrays togeter
+
+ /*const mergeArrays = (arrToBeMerged, arrToBeMerged2)=> {
+    for(let i = 0; i < arrToBeMerged.length; i++) {
+        arrToBeMerged2.push(arrToBeMerged[i])
+        
+    } 
+   return arrToBeMerged
+
+}
+const mergedWithALoop = mergeArrays(names1, names2)
+console.log('merged with a loop and push', mergedWithALoop)//Please check this method out */
+
+//use SOME to check if there is a number higher than  4 in the numbersArr.
+
+const numberHigherThan4 = numbersArr.some((item) => {
+   return  item > 4
+})
+console.log('Is there a number higher than 4? ', numberHigherThan4)
+
+//use EVERY to check, if all numbers in the numbersArr are lower than 1000
+
+const lowerThan1000 = numbersArr.every((item) => item < 1000)
+console.log('Are all the numbers lower than 1000? ', lowerThan1000)
+
+// Let's try forEach for the same thing and see the results
+const doTheSameAsEveryWithForEach = numbersArr.forEach((item) => {
+    if(item > 1000) {
+        console.log('All numbers are lower than 1000')
+    } else {
+        console.log('Not all numbers are lower than 1000')
+    }
+})
